9c649c384c85bf624190dcb2134601473a344ed8
jokers963 <jokers963@users.noreply.github.com>
2026-09-22 11:11:22 +0800

    Revert SupJav verification wait

--- js/supjav.user.js
+++ js/supjav.user.js
@@ -100,14 +100,8 @@
     };
 
     let sent = false;
-    let retries = 0;
     function sendResult() {
         if (sent) return;
-        if (!document.querySelector(".post") && retries++ < 45) {
-            GmSpiderInject.ShowWebview();
-            setTimeout(sendResult, 1000);
-            return;
-        }
         sent = true;
         const result = spider[method].apply(spider, args);
         GmSpiderInject.HideWebview();
